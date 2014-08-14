465be5d00ca6f42f69a6c67b069e5e63edbd3759
Mini Fu <fu.smile.android@gmail.com>
2014-08-14 15:59:03 +0800

    add ip check

--- net/server.js
+++ net/server.js
@@ -4,56 +4,60 @@ var users={};
 //users['server']=1;
 var server = net.createServer(function(conn){
 	conn.setEncoding('utf8');
-	
-	var nickname;
 	console.log('\033[90m new connection ! \033[39m');
 	conn.write('\033[92m welcome hello message \033[39m'
 		+ '\033[97m'+ count + '\033[39m' 
 		+ '\033[92m connections \033[39m');
 		count++;
+	var d =new Date();	
+	console.log(d);
+	var nickname=conn.remoteAddress;
+	if(users[nickname]){	
+		conn.write('\033[93m'+d.getTime()+'>'+ nickname +' already in use, try again: \033[39m');
+		console.log('\033[93m'+d.getTime()+'>'+ nickname +' already in use, try again: \033[39m');
+		for(var j=1;;j++)
+		{
+			nickname=nickname+'-'+j;
+			if(users[nickname])
+				continue;
+			else
+			{
+				users[nickname] = conn;
+				console.log('\033[90m'+d.getTime()+'>'+nickname+' joined the room \033[39m\n');
+				for(var i in users){
+					users[i].write('\033[90m '+d.getTime()+'>'+nickname+' joined the room \033[39m\n');
+					
+				}
+				break;
+			}
+		}
+	}else{
+		//nickname=data;
 		
-
+		users[nickname] = conn;
+		for(var i in users){
+			users[i].write('\033[90m '+d.getTime()+'>'+nickname+' joined the room \033[39m\n');
+			console.log('\033[90m '+d.getTime()+'>'+nickname+' joined the room \033[39m\n');
+		}
+	}
+	
+		
+	
 	conn.on('close',function(d){
 		console.log('close'+d);	
 	});
-	
-	conn.on('data',function(data){
-		
-		
-		data=data.replace('\r\n','');
-		if(!nickname){
-			
-			if(users[data]){
-			//	console.log('1'+data+'1'+users[data]);
-				conn.write('\033[93m> nickname already in use, try again: \033[39m');
-				console.log('\033[93m> nickname already in use, try again: \033[39m');
-				return;
-			}else{
-				nickname=data;
-				
-				users[nickname] = conn;
-				for(var i in users){
-					users[i].write('\033[90m >'+nickname+' joined the room \033[39m\n');
-					console.log('\033[90m >'+nickname+' joined the room \033[39m\n');
-				}
-			}
 
-	//		console.log('if(!nickname=!',data);
-		}else{
-	//		console.log('nickname='+nickname);
-			console.log('\033[96m >'+nickname+': \033[39m'+data);
+	conn.on('data',function(data){
+		var d =new Date();
+		data=data.replace('\r\n','');
+			console.log('\033[96m '+d.getTime()+'>'+nickname+': \033[39m'+data);
 			for(var i in users){
-				//if (i!=nickname)
-				{	
-					users[i].write('\033[96m >'+nickname+': \033[39m'+data+ '\n');
-				}
-			}
-		}
+					users[i].write('\033[96m '+d.getTime()+'>'+nickname+': \033[39m'+data+ '\n');
+			}		
 	});
+
 	conn.on('close',function(){
 		count--;
-		
-		
 	});
 });
 
