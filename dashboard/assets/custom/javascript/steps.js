d11e16af3f6cc0b45aafa2993f021ea949fafb2c
BrianHsu <brianhsu.hsu@gmail.com>
2014-09-09 14:49:26 +0800

    Add step title controller API.

--- dashboard/assets/custom/javascript/steps.js
+++ dashboard/assets/custom/javascript/steps.js
@@ -10,10 +10,21 @@ function resetSteps() {
 
 }
 
-function setStepsTitle(idPrefix, titles) {
+function setStepsTitle(idPrefix, titles, shouldSelectLast) {
+  resetSteps();
+
+  $("#" + idPrefix + "0").addClass('active');
+
   for (var i = 0; i < titles.length; i++) {
     var stepNode = "#" + idPrefix + (i + 1);
+    $(stepNode).addClass('active');
     $(stepNode).text(titles[i]);
   }
+
+  if (shouldSelectLast) {
+    var lastStepNode = '#' + idPrefix + (titles.length + 1);
+    $(lastStepNode).addClass("active");
+  }
+
 }
 
