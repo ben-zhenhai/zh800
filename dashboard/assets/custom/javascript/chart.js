3a2e1d6180ca2d00ed2439ee726f8b44a90d2668
BrianHsu <brianhsu.hsu@gmail.com>
2014-09-09 16:11:50 +0800

    Add comments in chart.js for bar chart API usage.

--- dashboard/assets/custom/javascript/chart.js
+++ dashboard/assets/custom/javascript/chart.js
@@ -1,3 +1,26 @@
+/**
+ *  Create a function that colud draw a bar chart.
+ *
+ *  This function will return another function that could draw a pie chart
+ *  with siganature of function (selection, dataSet, onClickCallback), where
+ *  selection, dataSet is required and onClickCallback is optional.
+ *
+ *  onClickCallback should have the signature of function(data), where data
+ *  will be the data from clicked bar.
+ *
+ *  The bar chart will bind to the node that selected by selection parameter,
+ *  which should be a valid d3 / jquery selector.
+ *
+ *  The options parameter will control the basic attribute of this chart, and
+ *  must have the following attribute:
+ *
+ *   -  options.totalHeight
+ *   -  options.barWidth
+ *   -  options.barPadding
+ *   -  options.bottomMargin
+ *   -  options.topMargin
+ *
+ */
 function barChart(options) {
 
   function assertInt(variable, message) {
@@ -27,6 +50,14 @@ function barChart(options) {
       return options.extractValue(data); 
     }
 
+    function calculateRectYPosition(data) {
+      return options.totalHeight - scalar(getDataValue(data)) - options.bottomMargin-10;
+    }
+
+    function calculateTopLabelYPosition(data) {
+      return options.totalHeight - scalar(getDataValue(data)) - options.topMargin;
+    }
+
     var totalWidth = getXOffset(dataSet.length);
     var minimalY = options.bottomMargin;
     var maximalY = options.totalHeight - 
@@ -47,20 +78,14 @@ function barChart(options) {
     var bar = chart.selectAll("g").data(dataSet).enter().
                     append("g")
 
-    function calculateRectYPosition(data) {
-      return options.totalHeight - scalar(getDataValue(data)) - options.bottomMargin-10;
-    }
-
-    function calculateTopLabelYPosition(data) {
-      return options.totalHeight - scalar(getDataValue(data)) - options.topMargin;
-    }
-
+    // Bar rectangular
     bar.append("rect").
         attr("width", options.barWidth - 5).
         attr("height", function(d) { return scalar(getDataValue(d)) }).
         attr("x", function(d, i) { return getXOffset(i) }).
         attr("y", calculateRectYPosition)
 
+    // The top label, which is the actually data value
     bar.append("text").
         attr("x", function(d, i) { return getXOffset(i) }).
         attr("y", calculateTopLabelYPosition).
@@ -68,6 +93,7 @@ function barChart(options) {
         attr("dx", "20px").
         text(options.extractValue)
 
+    // The bottom label, the name of current data
     bar.append("text").
         attr("x", function(d, i) { return getXOffset(i) }).
         attr("y", options.totalHeight-10).
