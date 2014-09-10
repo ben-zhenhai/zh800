04bc3accc35a08424244007189712185c858dbfb
HsuHsiaoHsuan <hsuhsiaohsuan+github@gmail.com>
2014-09-10 13:35:55 +0800

    [2014/09/10]     modify pressureTesh.sh to write real ip into database

--- mongodb/pressureTest.sh
+++ mongodb/pressureTest.sh
@@ -3,6 +3,11 @@
 while true
 do
     now=$(date +"%I:%M:%S")
-    node SendDataClient.js 01	BL20135	1000000	1	"$now"	1	0.0.0.0	3	E35	6957	0	0	0	03
+    #hostname=$(hostname)
+    ip=$(ifconfig eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+    node SendDataClient.js 01	BL20135	1000000	1	"$now"	1	"$ip"	3	E35	6957	0	0	0	03
+    #echo $now
+    #echo $hostname
+    #echo $ip
     #sleep 3
 done
