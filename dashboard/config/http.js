80e6201f6faa405d0dd115ddf48810304aa286d6
BrianHsu <brianhsu.hsu@gmail.com>
2014-09-19 17:21:04 +0800

    Move customMiddleware to correct file.

--- dashboard/config/http.js
+++ dashboard/config/http.js
@@ -84,4 +84,13 @@ module.exports.http = {
   ***************************************************************************/
 
   // cache: 31557600000
+  //
+  customMiddleware: function (app) {
+    var logger = require('morgan');
+    var fs = require('fs');
+    //var accessLogStream = fs.createWriteStream(__dirname + '/../access.log', {flags: 'a'})
+    var accessLogStream = fs.createWriteStream('/dev/null', {flags: 'a'})
+
+    app.use(logger('combined', {stream: accessLogStream}));
+  }
 };
