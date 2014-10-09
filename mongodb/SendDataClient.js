09dd68dc7d80ee0602be18f5e003eb65642e4ad6
HsuHsiaoHsuan <hsuhsiaohsuan+github@gmail.com>
2014-10-09 18:45:43 +0800

    [2014/10/09]     modifide SendDataClient

--- mongodb/SendDataClient.js
+++ mongodb/SendDataClient.js
@@ -1,11 +1,79 @@
 var net = require('net')
-var tcpClient = new net.Socket()
+var tcpClient =  net.Socket()
 
 var PORT = 5566
-var HOST = '192.168.1.30'
+var HOST = '192.168.0.181'
 
 var args = process.argv.slice(2)
+var fs = require('fs')
 
+var line = ''
+var index = 0
+var last = 0
+
+function readLine(input, func) {
+    var remaining = ''
+
+    console.log('HI archer');
+    input.on('data', function(data) {
+        remaining += data;
+        index = remaining.indexOf('\n');
+        //var index = remaining.indexOf('\n');
+        last  = 0;
+        //var last  = 0;
+        //while (index > -1) {
+        if (index > -1) {
+            //var line = remaining.substring(last, index);
+            line = remaining.substring(last, index);
+            last = index + 1;
+            func(line);
+            index = remaining.indexOf('\n', last);
+        }
+        //}
+        //remaining = remaining.substring(last);
+    });
+
+    input.on('end', function() {
+        if (remaining.length > 0) {
+            func_two(remaining);
+        }
+    });
+}
+
+function func(data) {
+    console.log('HI' + data);
+    tcpClient.connect(
+        PORT, HOST,
+        function() {
+            console.log('Connected to ' + HOST + ':' + PORT)
+            console.log('Start to send data: ' + data)
+            tcpClient.write(data,function(){
+                console.log('callback~~~~~~~')
+                readLine(inputFile, func)
+            })
+            tcpClient.destroy()
+            console.log('destroy')
+        }
+    )
+}
+tcpClient.on('data', function() {
+        console.log('data')
+    }
+)
+
+tcpClient.on('end', function() {
+        console.log('test')
+    }
+)
+
+
+function func_two(data) {
+    console.log(data)
+}
+
+var inputFile = fs.createReadStream(args.toString())
+readLine(inputFile, func)
+/*
 tcpClient.connect(
     PORT, HOST, 
     function() {
@@ -15,15 +83,15 @@ tcpClient.connect(
         tcpClient.destroy()
     }
 )
-
 /*
+
 tcpClient.on(
     'data', 
     function(data) {
         console.log('Received: ' + data.toString())
     }
 )
-*/
+
 
 tcpClient.on(
     'close', 
@@ -56,3 +124,4 @@ function reConnect() {
 
     console.log('will try to reconnect to ' + HOST + ':' + PORT)
 }
+*/
