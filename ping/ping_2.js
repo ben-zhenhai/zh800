63f74e53cc57c2afbc6ed37703380c7bcbb7288b
HsuHsiaoHsuan <hsuhsiaohsuan+github@gmail.com>
2014-11-06 17:31:16 +0800

    [2014/11/06]     1. show ID on web page

--- ping/ping_2.js
+++ ping/ping_2.js
@@ -1,3 +1,4 @@
+var jsonQuery = require('json-query');
 var ping = require('net-ping');
 var options = {
   networkProtocol: ping.NetworkProtocol.IPv4,
@@ -10,17 +11,36 @@ var options = {
 var session = ping.createSession(options);
 var pingSet = {};
 var fs = require('fs');
+var ipMappingFile = 'ipMapping.json';
+var ipMappingData = {};
+
+fs.readFile(ipMappingFile, 'utf8', function(err, data) {
+  if(err) {
+    console.log('read ipMappingFile error');
+    return;
+  }
+  ipMappingData = JSON.parse(data);
+  
+  //console.log(ipMappingData);
+  //var tmp = ipMappingData.filter(function(obj) {
+  //  return obj.IP === '192.168.10.2';
+  //});
+  //console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>>>>' + tmp[0].ID);
+});
 
 process.on('message', function(msg) {
-  console.log('child get ' + msg);
+  //console.log('child get ' + msg);
   if(msg == 'firstInit') {
     checkAliveResult(10);
   }
 });
 
-function initIpArray(ipRange, upper, lower) {
+function initIpArray(ipRange, upper, lower, ignoreIPs) {
   for (var x = parseInt(lower); x <= parseInt(upper); x++) {
     pingSet[ipRange+x] = 0;
+  }
+  for(var x = 0; x < ignoreIPs.length; x++) {
+    delete pingSet[ipRange+ignoreIPs[x]];
   }
 }
 
@@ -43,9 +63,15 @@ function checkAlive() {
 function checkAliveResult(errorTimes) {
   var failedIp = [];
   for(var ip in pingSet) {
-    console.log(ip + '  ' + pingSet[ip] + ' ' + errorTimes);
-    if(pingSet[ip] >= errorTimes) {
+    //console.log(ip + '  ' + pingSet[ip] + ' ' + errorTimes);
+    if(pingSet[ip] >= errorTimes) { 
+      var tmpID = ipMappingData.filter(function(obj) {
+        return obj.IP === ip;
+      });
       var data = {};
+      //console.log('>>>>>>>>>>>>>> ' + ip);
+      data["ID"] = tmpID[0].ID;
+      //console.log('~~~~~~~~>>>' + data["ID"]);
       data["IP"] = ip;
       data["DATE"] = Date();
       failedIp.push(data);
@@ -56,8 +82,9 @@ function checkAliveResult(errorTimes) {
 }
 
 var iniRead = require('./iniRead.js')(function(iniData) {
-  initIpArray(iniData.ipRange, iniData.ipRange_upper, iniData.ipRange_lower);
-  initIpArray(iniData.ipRange2, iniData.ipRange2_upper, iniData.ipRange2_lower);
+  //console.log(iniData.ignoreIP.ip[0]);
+  initIpArray(iniData.ipRange1.range, iniData.ipRange1.upper, iniData.ipRange1.lower, iniData.ipRange1.ignore);
+  initIpArray(iniData.ipRange2.range, iniData.ipRange2.upper, iniData.ipRange2.lower, iniData.ipRange2.ignore);
   setInterval(checkAlive, iniData.pingTime);
   setInterval(checkAliveResult, iniData.checkTime, iniData.errorTimes);
 });
