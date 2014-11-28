51bb9a365a2cdab6431c183ed63610bacd80f666
BrianHsu <brianhsu.hsu@gmail.com>
2014-11-28 12:11:53 +0800

    Remove dashboard folder, move it to separate GitHub repo.

--- dashboard/api/policies/redirectToDashboardIfLogin.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (req, res, next) {
-  
-  if (req.session.authenticated) {
-    res.redirect("/dashboard")
-    return;
-  } else {
-    return next();
-  }
-}
--- dashboard/api/services/PaddingZero.js
+++
@@ -1,7 +0,0 @@
-exports.padding = function paddingZero(number) {
-  if (+number < 10) {
-    return "0" + number;
-  } else {
-    return number;
-  }
-}
