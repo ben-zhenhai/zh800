602862ebee3295e22edf99bb130a0672d7e49fe3
zhenhai-server <zhenhai@zhenhai.com.tw>
2015-08-19 12:15:32 +0800

    [2015/08/19]     1. remove FreemanTest/CameraOpenCV

--- camera/FreemanTest/CameraOpenCV/Debug/src/CameraOpenCV.d
+++
@@ -1,8 +0,0 @@
-src/CameraOpenCV.d: ../src/CameraOpenCV.c ../src/ZhCvCommon.h \
- /usr/local/include/opencv/cv.h /usr/local/include/opencv/highgui.h
-
-../src/ZhCvCommon.h:
-
-/usr/local/include/opencv/cv.h:
-
-/usr/local/include/opencv/highgui.h:
--- camera/FreemanTest/CameraOpenCV/Debug/src/ZhCvCommon.d
+++
@@ -1,8 +0,0 @@
-src/ZhCvCommon.d: ../src/ZhCvCommon.c ../src/ZhCvCommon.h \
- /usr/local/include/opencv/cv.h /usr/local/include/opencv/highgui.h
-
-../src/ZhCvCommon.h:
-
-/usr/local/include/opencv/cv.h:
-
-/usr/local/include/opencv/highgui.h:
